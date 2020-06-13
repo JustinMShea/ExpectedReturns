a5d9db9bcc9d54bdd9a969b6be71415bd5b8c1a2
JustinMShea <jshea@roosevelt.edu>
2020-06-13 13:34:40 -0500

    Test on single equity removed, as my patch was accepted for FactorAnalytics

--- inst/tinytest/test_fitFfm.R
+++ inst/tinytest/test_fitFfm.R
@@ -10,20 +10,6 @@ fit.test <- fitFfm(data=factorDataSetDjia5Yrs, asset.var="TICKER", ret.var="RETU
 expect_identical(class(fit.test), "ffm")
 
 
-## On a single equity AA
-test.data.aa <- factorDataSetDjia5Yrs[which(factorDataSetDjia5Yrs[, 'TICKER'] == 'AA'), ]
-
-fit.test.aa <- fitFfm(data=test.data.aa, asset.var="TICKER", ret.var="RETURN",
-                      date.var="DATE", exposure.vars=c("MKTCAP", "ENTVAL", "SIZE"),
-                      z.score="crossSection", addIntercept=TRUE)
-
-expect_error(class(fit.test.aa), "object 'fit.test.aa' not found")
- # lm on a single equity AA works fine ####
-lm.test.aa <- lm(RETURN ~ MKTCAP + ENTVAL + SIZE, data = test.data.aa)
-
-expect_identical(class(lm.test.aa), "lm")
-
-
 ## On two equities AA, BA
 index <- factorDataSetDjia5Yrs$TICKER == 'AA' | factorDataSetDjia5Yrs$TICKER == 'BA'
 test.data.aa.ba <- factorDataSetDjia5Yrs[index, ]
