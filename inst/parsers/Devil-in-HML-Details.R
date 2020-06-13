b5a8709302b7561dc059ca2b1c412150c350933d
JustinMShea <jshea@roosevelt.edu>
2020-06-12 23:53:53 -0500

    AQR HML Devils, Total Market Value of Equity factors #8

--- inst/parsers/Devil-in-HML-Details.R
+++ inst/parsers/Devil-in-HML-Details.R
@@ -17,19 +17,24 @@ HML_Devil.ExcessReturns <- read.xlsx(path, sheet = 1, startRow = 18, colNames = 
 variable.names <- read.xlsx(path, sheet = 1, startRow = 17)
 colnames(HML_Devil) <- variable.names[1,]
 rm(variable.names)
-  # Format
 HML_Devil$DATE <- as.Date(HML_Devil$DATE, format = "%m/%d/%Y")
 
  ## Up Minus Down (UMD) factors
-HML_Devil.UMD <- read.xlsx(path, sheet = 8, startRow = 18, colNames = TRUE)
+HML_Devil.UMD <- read.xlsx(path, sheet = 8, startRow = 18, colNames = FALSE)
   # NOTE: Due to .xlsx formatting, can't read in with column names automatically
 variable.names <- read.xlsx(path, sheet = 8, startRow = 17)
 colnames(HML_Devil.UMD) <- variable.names[1,]
 rm(variable.names)
-  # Format
 HML_Devil.UMD$DATE <- as.Date(HML_Devil.UMD$DATE, format = "%m/%d/%Y")
 
+## Total Market Value of Equity (ME) factors, lagged 1 month (Billion USD)
+HML_Devil.ME_1 <- read.xlsx(path, sheet = 9, startRow = 19, colNames = FALSE)
+# NOTE: Due to .xlsx formatting, can't read in with column names automatically
+variable.names <- read.xlsx(path, sheet = 9, startRow = 18)
+colnames(HML_Devil.ME_1) <- variable.names[1,]
+rm(variable.names)
+HML_Devil.ME_1$DATE <- as.Date(HML_Devil.ME_1$DATE, format = "%m/%d/%Y")
 
-# Save to .RData file if we decide/get permissions.
-# save(HML_Devil, file = paste0("data/AQR_HML_Devil.RData"), compress = "xz", compression_level = 9)
+
+
 
