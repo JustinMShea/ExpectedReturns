0191723cd29717883a73ea8b2dfd2110c10d3228
JustinMShea <jshea@roosevelt.edu>
2020-06-13 09:31:22 -0500

    clean up existing tests, added another test on 2 equities.

--- inst/tinytest/test_fitFfm.R
+++ inst/tinytest/test_fitFfm.R
@@ -1,23 +1,35 @@
 
 data(factorDataSetDjia5Yrs)
 
+
 ## test on full data set
 fit.test <- fitFfm(data=factorDataSetDjia5Yrs, asset.var="TICKER", ret.var="RETURN",
                    date.var="DATE", exposure.vars=c("MKTCAP", "ENTVAL", "SIZE"),
-                   z.score="crossSection",
-                   addIntercept=TRUE)
+                   z.score="crossSection", addIntercept=TRUE)
 
 expect_identical(class(fit.test), "ffm")
 
-## On a single stock
-test.data.aa <- test.data[which(test.data[, 'TICKER'] == 'AA'), ]
 
-fit.test.aa <- fitFfm(data=test.data.aa, asset.var="TICKER", ret.var="EXC.RETURN", date.var="DATE",
-                      exposure.vars=exposure.vars, lagExposures=TRUE, addIntercept=TRUE)
+## On a single equity AA
+test.data.aa <- factorDataSetDjia5Yrs[which(factorDataSetDjia5Yrs[, 'TICKER'] == 'AA'), ]
+
+fit.test.aa <- fitFfm(data=test.data.aa, asset.var="TICKER", ret.var="RETURN",
+                      date.var="DATE", exposure.vars=c("MKTCAP", "ENTVAL", "SIZE"),
+                      z.score="crossSection", addIntercept=TRUE)
 
 expect_error(class(fit.test.aa), "object 'fit.test.aa' not found")
-
-#### TEST :: lm on data confirms data is fine. Issue is ####
+ # lm on a single equity AA works fine ####
 lm.test.aa <- lm(EXC.RETURN ~ MKT.RF + SMB + HML, data = test.data.aa)
 
 expect_identical(class(lm.test.aa), "lm")
+
+
+## On two equities AA, BA
+index <- factorDataSetDjia5Yrs$TICKER == 'AA' | factorDataSetDjia5Yrs$TICKER == 'BA'
+test.data.aa.ba <- factorDataSetDjia5Yrs[index, ]
+
+fit.test.aa.ba <- fitFfm(data=test.data.aa.ba, asset.var="TICKER", ret.var="RETURN",
+                         date.var="DATE", exposure.vars=c("MKTCAP", "ENTVAL", "SIZE"),
+                         z.score="crossSection", addIntercept=TRUE)
+
+expect_identical(class(fit.test.aa.ba), "ffm")
