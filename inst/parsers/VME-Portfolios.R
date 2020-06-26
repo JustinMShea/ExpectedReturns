379db1869623f772cb41bca68902e8119fb1f3cc
Vito Lestingi <LestingiV@gmail.com>
2020-06-26 10:56:18 +0200

    Polish VME *portfolios* parser
    Adjusted the parser similarly to what did with the recently added one on VME factors.
    
    As a consequence, removed scattered conversions in vignette.
    
    #15

--- inst/parsers/VME-Portfolios.R
+++ inst/parsers/VME-Portfolios.R
@@ -7,17 +7,22 @@
 #
 # Source: https://www.aqr.com/Insights/Datasets/Value-and-Momentum-Everywhere-Portfolios-Monthly
 
-# Dowload file (in R already)
+## Download in R environment
 AQR.VME.Portfolios.url <- "https://images.aqr.com/-/media/AQR/Documents/Insights/Data-Sets/Value-and-Momentum-Everywhere-Portfolios-Monthly.xlsx"
-VME.Portfolios <- rio::import(AQR.VME.Portfolios.url)
-# Clean up
-cols.names <- 20
-variable.names <- as.character(VME.Portfolios[cols.names, ])
-start.data <- 21
-VME.Portfolios <- VME.Portfolios[start.data:nrow(VME.Portfolios), ]
-# Column names
+VME.Portfolios.raw <- rio::import(AQR.VME.Portfolios.url, format='xlsx')
+
+## Clean up
+header.row <- 20
+data.begin.row <- header.row + 1
+VME.Portfolios <- VME.Portfolios.raw[data.begin.row:nrow(VME.Portfolios.raw), ]
+rownames(VME.Portfolios) <- NULL
+
+variable.names <- as.character(VME.Portfolios.raw[header.row, ])
 variable.names <- gsub('_', '.', variable.names)
 colnames(VME.Portfolios) <- toupper(variable.names)
 rm(variable.names)
-# Convert to "Date"
+
+# Convert variables to "numeric" and dates to "Date"
+VME.Portfolios.vars <- colnames(VME.Portfolios) != 'DATE'
+VME.Portfolios[, VME.Portfolios.vars] <- apply(VME.Portfolios[, VME.Portfolios.vars], 2, as.numeric)
 VME.Portfolios$DATE <- as.Date.character(VME.Portfolios$DATE, '%m/%d/%Y')
