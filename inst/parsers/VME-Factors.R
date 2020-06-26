93ddd30df9d1f5e60eb603eb3013924ee9992ae1
Vito Lestingi <LestingiV@gmail.com>
2020-06-26 10:09:32 +0200

    Polish VME *factors* parser
    Adjusted the parser similarly to what did with the recently added one on TSM. In particular, switched to `rio` for data import.
    
    As a consequence, removed scattered conversions in vignette.
    
    #15

--- inst/parsers/VME-Factors.R
+++ inst/parsers/VME-Factors.R
@@ -7,24 +7,32 @@
 #
 # Source: https://www.aqr.com/Insights/Datasets/Value-and-Momentum-Everywhere-Factors-Monthly
 
-# Download to (your) sandbox/data/
+## Download in R environment
 AQR.VME.Factors.url <- "https://images.aqr.com/-/media/AQR/Documents/Insights/Data-Sets/Value-and-Momentum-Everywhere-Factors-Monthly.xlsx"
-path <- "sandbox/data/AQR.VME.Factors.xlsx"
-download.file(AQR.VME.Factors.url, path)
-# NOTE due to .xlsx formatting, can't read in with column names automatically
-VME.Factors <- openxlsx::read.xlsx(path, sheet=1, startRow=23, colNames=FALSE)
-# Remove empty cells (not NA)
-empty.idxs <- which(apply(VME.Factors == "", 1, all))
-incl.idxs <- setdiff(as.numeric(rownames(VME.Factors)), empty.idxs)
-VME.Factors <- VME.Factors[incl.idxs, ]
-# Column names
-variable.names <- openxlsx::read.xlsx(path, sheet=1, startRow=22)
-variable.names <- colnames(variable.names[1, ])
+# path <- "sandbox/data/AQR.VME.Factors.xlsx"
+# download.file(AQR.VME.Factors.url, path)
+# VME.Factors <- openxlsx::read.xlsx(path, sheet=1, startRow=23, colNames=FALSE)
+VME.Factors.raw <- rio::import(AQR.VME.Factors.url, format='xlsx')
+
+## Clean up
+header.row <- 21
+data.begin.row <- header.row + 1
+VME.Factors <- VME.Factors.raw[data.begin.row:nrow(VME.Factors.raw), ]
+
+variable.names <- as.character(VME.Factors.raw[header.row, ])
 variable.names <- gsub('\\^', '.', variable.names)
 variable.names <- gsub('_', '.', variable.names)
 variable.names <- sub("^VAL$", replacement="VAL.EVR", variable.names)
 variable.names <- sub("^MOM$", replacement="MOM.EVR", variable.names)
 colnames(VME.Factors) <- variable.names
 rm(variable.names)
-# Convert to "Date"
+
+# Convert variables to "numeric" and dates to "Date"
+VME.Factors.vars <- colnames(VME.Factors) != 'DATE'
+VME.Factors[, VME.Factors.vars] <- apply(VME.Factors[, VME.Factors.vars], 2, as.numeric)
 VME.Factors$DATE <- as.Date.character(VME.Factors$DATE, '%m/%d/%Y')
+
+# Remove empty cells
+row.names(VME.Factors) <- NULL
+data.end.row <- max(which(!is.na(VME.Factors$DATE)))
+VME.Factors <- VME.Factors[1:data.end.row, ]
