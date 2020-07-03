d5005294862cb8eba10346b3f7783a918ac63eaf
Vito Lestingi <LestingiV@gmail.com>
2020-07-03 16:44:40 +0200

    Create VIX close data parser (FRED)
    CBOE's VIX Index is used as a factor in TSM.
    
    VIX OHLC data from Yahoo Finance via `quantmod` is a too short series with respect to sample period of interest in TSM.
    
    Thankfully, `quantmod` also connects to FRED and FRED is a wonderful open resource for everyone. I was only able to find VIX closing prices, which nonetheless should suffice for TSM replication purposes.
    
    #17

---
+++ inst/parsers/VIX-FRED.R
@@ -0,0 +1,29 @@
+
+# CBOE Volatility Index: VIX (VIXCLS)
+#
+# Units: Index, Not Seasonally Adjusted
+#
+# Period: 1990-01-02 to present
+# (NOTE: not real-time, usually one day lag vs CBOE)
+#
+# Frequency: Daily, Close
+#
+# Source: https://fred.stlouisfed.org/series/VIXCLS
+
+## VIX OHLC via quantmod (from Yahoo) is convenient, but too short series
+# VIX.qm <- quantmod::getSymbols('^VIX', env=globalenv(), auto.assign=FALSE) # 2007-01-03 to date
+
+## VIX Monthly Close via quantmod (from FRED)
+VIX.cls.daily <- quantmod::getSymbols.FRED('VIXCLS', env=globalenv(), auto.assign=FALSE)
+vix.monthly.idxs <- xts::endpoints(VIX.cls.daily)
+VIX.cls.monthly <- VIX.cls.daily[vix.monthly.idxs, ]
+# NOTE: returns in decimal unit
+VIX.cls.monthly$VIX.RET <- PerformanceAnalytics::Return.calculate(VIX.cls.monthly$VIXCLS, 'discrete')
+VIX.cls.monthly$VIX.COMP.RET <- PerformanceAnalytics::Return.calculate(VIX.cls.monthly$VIXCLS, 'log')
+
+## Remove unused variables
+rm(
+  # VIX.qm,
+  VIX.cls.daily,
+  vix.monthly.idxs
+)
