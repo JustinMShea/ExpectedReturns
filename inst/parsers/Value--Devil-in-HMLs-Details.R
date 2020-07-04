ad7bcb3faad38e71101cba994db2a2cfee912b09
Vito Lestingi <LestingiV@gmail.com>
2020-07-04 17:45:22 +0200

    Let R CMD check build vignettes
    Modulo some LaTeX re-running warnings, devtools::build_vignettes() is able to construct vignettes with \VignetteEngine{knitr::rmarkdown}. Yet devtools::check() returns vignettes building errors. This does not appear to me to be a building error in the sense of <R CMD build>, but a <R CMD check> error. A wild guess I can attempt is that the check fails because the former builds from 'vignettes/', whereas the latter rebuilds from another (copied) directory (e.g. 'inst/doc/') and is then unable to find parsers' paths.
    
    Nonetheless, \VignetteEngine{rmarkdown::render} is maybe only a workaround and check results report a WARNING of the form:
    "Files named as vignettes but with no recognized vignette engine:
          ‘vignettes/*.Rmd’
       (Is a VignetteBuilder field missing?)
    Files named as vignettes but with no recognized vignette engine:
         ‘vignettes/*.Rmd’"
    The topics is being discussed right at https://github.com/r-lib/pkgbuild/issues/58.
    
    From Writing R Extensions: "When R CMD build builds the vignettes, it copies these and the vignette sources from directory vignettes to inst/doc. To install any other files from the vignettes directory, include a file vignettes/.install_extras which specifies these as Perl-like regular expressions on one or more lines. (See the description of the .Rinstignore file for full details.)" (https://cran.r-project.org/doc/manuals/r-release/R-exts.html#Writing-package-vignettes). Please note this however doesn't directly apply to our problem, as vignettes root.dir is at package root dir. We may be able to better manage the process via '.install_extras'.
    
    A related problem arising during checks is as follows. knitr, among other things, produces *.pdf and separates *.R "runnable" scripts from vignettes. As mentioned in 57f28ce, the "HML Devil" parser introduced by a98c0f8 is a potential source of problems. The file shares its name with the vignette, but to my knowledge this is also knitr's default behaviour when extracting runnable code, so what is going on would be that in the check process the file gets overwritten and hence never executes the parser. It should be why knitr is then understandably unable to find objects and as a consequence errors out. In order to show the effects of the attempted fix I'm momentarily copying @JustinMShea and @erolbicero's parser to inst/parsers/, where all the other parsers are.
    
    Please note, I am not 100% sure this is the ideal solution, but with the present commit the check is satisfied as opposed to clogged by apparently exotic "building" errors. In particular, it only produces 1 WARNING to my knowledge. Our current Travis config will fail builds WARNINGs, but at the same time '--no-build-vignettes' options are passed, so the vignette building WARNING is hidden to Travis and hence both local and Travis builds&checks should succeed.

---
+++ inst/parsers/Value--Devil-in-HMLs-Details.R
@@ -0,0 +1,95 @@
+# The Devil in HML's Details: Factors, Monthly
+## https://www.aqr.com/Insights/Datasets/The-Devil-in-HMLs-Details-Factors-Monthly
+
+###--------------------------------------------------------------------------------#
+## WARNING: Some countries factor returns parsed as char vectors. Needs formating ##
+#--------------------------------------------------------------------------------###
+
+#Define Import Function
+importRemoteAQRxlsxFile <- function(path, sheet = 1, startRow = 17, removeBlankRow = TRUE, dateFormat = "%m/%d/%Y"){
+
+  #import data
+  rawImportData <- rio::import(file = path, readxl = FALSE, sheet = sheet, startRow = startRow, colNames = FALSE)
+
+  if(removeBlankRow){
+      #remove first "blank" row
+      rawImportData <- rawImportData[-1,]
+  }
+
+  #set column/variable names
+  colnames(rawImportData) <- rawImportData[1,]
+
+  #remove column/variable names from dataset
+  rawImportData <- rawImportData[-1,]
+
+  # head(rawImportData)
+
+  #format date
+  rawImportData$DATE <- as.Date(rawImportData$DATE, format = dateFormat)
+
+  #format all other values as numeric
+  rawImportData[,-1] <-  switch(
+                              class(rawImportData[,-1])
+                            , data.frame = {apply(rawImportData[,-1],2,as.numeric)}
+                            , {as.numeric(rawImportData[,-1])}
+                              )
+
+  #reset rownames
+  rownames(rawImportData) <- NULL
+
+  return(rawImportData)
+
+}
+
+## Load Libraries
+library(rio)
+library(openxlsx)
+
+## Download to Sandbox
+
+AQR_HML_Devil_file <- "https://images.aqr.com/-/media/AQR/Documents/Insights/Data-Sets/The-Devil-in-HMLs-Details-Factors-Monthly.xlsx"
+
+## Load data from Sandbox
+### 1. Long/short High Minus Low Devil (HML Devil) factors
+
+# NOTE (Vito): Switched 'HML_Devil.ExcessReturns' to 'HML_Devil.HML.DEV' as other factors.
+#              If you already have your own code in place run:
+#              HML_Devil.ExcessReturns <- HML_Devil.HML.DEV
+HML_Devil.HML.DEV <- importRemoteAQRxlsxFile(path = AQR_HML_Devil_file, sheet = "HML Devil")
+
+### 2. Fama-French Factors
+# NOTE (Vito): Switched 'HML_Devil.Mkt' to 'HML_Devil.MKT' for consistency with previous code.
+#              If you already have your own code in place run:
+#              HML_Devil.Mkt <- HML_Devil.MKT
+HML_Devil.MKT <- importRemoteAQRxlsxFile(path = AQR_HML_Devil_file, sheet = "MKT")
+
+HML_Devil.SMB <- importRemoteAQRxlsxFile(path = AQR_HML_Devil_file, sheet = "SMB")
+
+HML_Devil.HML_FF <- importRemoteAQRxlsxFile(path = AQR_HML_Devil_file, sheet = "HML FF")
+
+### 3. Up Minus Down (UMD) factors
+HML_Devil.UMD <- importRemoteAQRxlsxFile(path = AQR_HML_Devil_file, sheet = "UMD")
+
+### 4. Total Market Value of Equity (ME) factors, lagged 1 month (Billion USD)
+HML_Devil.ME_1 <- importRemoteAQRxlsxFile(path = AQR_HML_Devil_file, sheet = "ME(t-1)")
+
+### 5. RF: U.S. Treasury bill rates
+HML_Devil.RF <- importRemoteAQRxlsxFile(path = AQR_HML_Devil_file, sheet = "RF", removeBlankRow = FALSE)
+
+## Add script to merge factor data by country below ##
+
+## Remove Import Function
+rm(importRemoteAQRxlsxFile)
+
+## Check
+# lapply(
+# ls()[sapply(sapply(ls(),function(x){gregexpr("HML_",x)}),`[`,1) == 1]
+# , function(variable){
+#   variable <- get(variable,envir = .GlobalEnv)
+#   sumVars <-
+#   switch(class(variable[,-1])
+#          , data.frame = apply(variable[,-1],2,sum,na.rm=TRUE)
+#          , sum(variable[,-1])
+#   )
+#   sumVars <- sum(sumVars)
+# })
