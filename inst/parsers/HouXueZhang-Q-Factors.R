cd4815dbf40fd95ae035c1863f10002ab7b13138
Vito Lestingi <LestingiV@gmail.com>
2020-08-20 19:37:20 +0200

    Load Q4 model factors data
    Q4, Hou-Xue-Zhang q4 factor model ("q-factor"), data set has been:
    - downloaded with GetFactors();
    - documented with info retrieval (initial docs);
    - pushed to 'data/' as an '.RData' file.
    
    This data set is needed for analyses and comparison in #23.
    
    #16

--- inst/parsers/HouXueZhang-Q-Factors.R
+++ inst/parsers/HouXueZhang-Q-Factors.R
@@ -8,9 +8,13 @@
 ## Q5 model data
 Q5.monthly <- ExpectedReturns::GetFactors('Q5', 'HXZ', freq='monthly')
 
+## Q4 model data
+Q5.monthly$EG <- NULL
+Q4.monthly <- Q5.monthly
+
 # Save data sets
 # NOTE: save to sandbox if needed
-objs.names <- 'Q5.monthly' # c('Q4.monthly', 'Q5.monthly')
+objs.names <- c('Q4.monthly', 'Q5.monthly')
 for (obj in objs.names) {
   save(
     list = obj,
