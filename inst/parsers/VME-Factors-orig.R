5e8f2f4803ecbf1bcf389334d0bb7bbe0d5001ba
Bryan Rodriguez <bryanrod182@gmail.com>
2022-07-25 01:07:56 -0500

    Added some notes. Added 'VME.Factors.orig.R' parser from original data used in paper downloaded from AQR. Kept the VME.Factros parser used before since it has the updated, most recent data.
    Data in parsers do differ even on the dates that overlap with the dates used in original VME paper. Excel sheet has a note listed warning that values may differ since methodology used to calculate returns in the most recent data may have differed from methodology authors used in VME paper 2013.

---
+++ inst/parsers/VME-Factors-orig.R
@@ -0,0 +1,29 @@
+# Value momentum everywhere, monthly factors from original source used on paper
+
+# Last updated by AQR on February 27th, 2018
+
+# Period 1972-01-31/2011-07-31
+
+# Source: https://images.aqr.com/Insights/Datasets/Value-and-Momentum-Everywhere-Factors-Monthly
+
+##Download data to R
+AQR.VME.Factors.orig.url <- "https://images.aqr.com/-/media/AQR/Documents/Insights/Data-Sets/Value-and-Momentum-Everywhere-Original-Paper-Data.xlsx"
+VME.Factors.orig <- openxlsx::read.xlsx(AQR.VME.Factors.orig.url, sheet = 2, startRow = 15, colNames = TRUE, detectDates =  TRUE)
+
+## Clean up
+variable.names <- colnames((VME.Factors.orig))
+variable.names <- gsub('\\^', '.', variable.names)
+variable.names <- sub("^VAL$", replacement = "VAL.EVR", variable.names)
+variable.names <- sub("^MOM$", replacement = "MOM.EVR", variable.names)
+colnames(VME.Factors.orig) <- variable.names
+
+## Remove NA's
+VME.Factors.orig <- zoo::na.trim(VME.Factors.orig)
+
+## Convert to xts
+VME.Factors.orig <- xts::xts(VME.Factors.orig[,-1], order.by = VME.Factors.orig$DATE)
+
+## Remove unused variables
+rm(AQR.VME.Factors.orig.url,
+   variable.names
+)
