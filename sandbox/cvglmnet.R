ca0fa8d1212af83c56852bf6b92c98f432d8a868
hfheziyu <zih028@ucsd.edu>
2024-06-20 12:17:03 -0500

    Update cvglmnet.R

--- sandbox/cvglmnet.R
+++ sandbox/cvglmnet.R
@@ -11,21 +11,25 @@ test_cvglmnet <- function(training_data,
                           testing_data,
                           y,
                           alpha = 0.5) {
-  x_test <- as.matrix(testing_data[, !y])
+  x_test <- as.matrix(testing_data[, !..y])
 
   mlr_enet <- cvglmnet(training_data, y, alpha)
   mlr_enet_fitted <- mlr_enet$model
   mlr_enet_lambda_min <- mlr_enet_fitted$lambda.min
 
-  mlr_enet_predict <- predict(fitted_lasso, x_test, mlr_enet_lambda_min)
+  mlr_enet_predict <- predict(mlr_enet_fitted, newx = x_test, s = mlr_enet_lambda_min)
 
-  x_train <- as.matrix(training_data[, !y])
-  y_train <- training_data[[y_var]]
+  x_train <- as.matrix(training_data[, !..y])
+  y_train <- training_data[[y]]
 
   glm_enet <- cv.glmnet(x_train, y_train, alpha = alpha)
-  glm_enet_lambda_min <- mlr_enet$lambda.min
+  glm_enet_lambda_min <- glm_enet$lambda.min
 
-  glm_predict <- predict(glm_enet, s = glm_enet_lambda_min, newx = x_test)
+  glm_predict <- predict(glm_enet, newx = x_test, s = glm_enet_lambda_min)
 
-  return(all.equal(mlr_enet_predict, glm_predict))
+  if (identical(mlr_enet_predict, glm_predict)) {
+    print("cv.glmnet test passed.")
+  } else {
+    stop("Warning: cv.glmnet test failed.")
+  }
 }
