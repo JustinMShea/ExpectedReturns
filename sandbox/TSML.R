5d007db73f1625ad9498b2b5a2349de13996e23c
hfheziyu <zih028@ucsd.edu>
2024-06-21 12:11:53 -0500

    Update TSML.R

--- sandbox/TSML.R
+++ sandbox/TSML.R
@@ -4,23 +4,30 @@ library(data.table)
 TSML <- R6Class("Time Series Machine Learning",
   public = list(
     data = NULL,
+    task = NULL,
     ts_var = NULL,
     y = NULL,
     cs_var = NULL,
     train_data = NULL,
     test_data = NULL,
     model = NULL,
+    learner = NULL,
     benchmark = list(),
-    predictions = NULL,
-    rmse = NULL,
+    prediction = NULL,
+    evals = NULL,
 
-    initialize = function(data, ts_var, y, cs_var = NULL) {
+    initialize = function(data, task, ts_var, y, cs_var = NULL) {
       if (!is.data.table(data)) {
         tryCatch({
           data <- as.data.table(data)
         }, error = function(e) {
           stop(paste("Error: data conversion to data.table failed:", e$message))
         })
+      }
+
+      task <- toupper(task)
+      if (!task %in% c("REGRESSION", "CLASSIFICATION")) {
+        stop(paste("Error: must specify whether it is a regression or a classification task."))
       }
 
       if (is.null(ts_var)) {
