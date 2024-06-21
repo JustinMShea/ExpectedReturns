53d2509df726af2faa5ac2f0da22d676f9b60612
hfheziyu <zih028@ucsd.edu>
2024-06-21 09:05:20 -0500

    Update outliers.R

--- sandbox/outliers.R
+++ sandbox/outliers.R
@@ -93,6 +93,8 @@ winsorize <- function(data,
   # Add to "ts" and "cs" method for panel data. Allow outlier detection by time-series and cross-sectional variables
 }
 
+
+# Pipeline Functions
 TSML$set("public", "interval_outlier", function(m = 5,
                                                 varnames = NULL,
                                                 by = "default",
@@ -111,9 +113,6 @@ TSML$set("public", "interval_outlier", function(m = 5,
     col_sd <- sd(data[[variable]], na.rm = TRUE)
     interval <- c(col_mean - m * col_sd, col_mean + m * col_sd)
     outlier_idx <- which(data[[variable]] < interval[1] | data[[variable]] > interval[2])
-    anomaly_data <- copy(data)
-    anomaly_data[, anomaly := FALSE]
-    anomaly_data[outlier_idx, anomaly := TRUE]
     p <- ggplot(data, aes_string(x = self$ts_var, y = variable, color = "variable values")) +
       geom_line() +
       geom_point(data = anomaly_data[anomaly == TRUE], aes_string(x = self$ts_var, y = variable, color = "anomaly")) +
@@ -128,31 +127,44 @@ TSML$set("public", "interval_outlier", function(m = 5,
   if (filter) {
     self$data <- self$data[-remove_row, ]
   }
-  return(list(data = data, outlier_dates = outlier_dates, remove_index = remove_row, plots = plots))
+  return(list(outlier_dates = outlier_dates, remove_index = remove_row, plots = plots))
+})
 
+TSML$set("public", "winsorize", function(q = 0.01,
+                                         varnames = NULL,
+                                         by = "default",
+                                         winsorize = FALSE,
+                                         plot = FALSE) {
+  if (is.null(varnames)) {
+    varnames <- setdiff(colnames(self$data), c(self$ts_var, self$cs_var))
+  }
 
-  for (variable in varnames) {
-    col_mean <- mean(data[[variable]], na.rm = TRUE)
-    col_sd <- sd(data[[variable]], na.rm = TRUE)
-    interval <- c(col_mean - m * col_sd, col_mean + m * col_sd)
-    outlier_idx <- which(data[[variable]] < interval[1] | data[[variable]] > interval[2])
-    anomaly_data <- copy(data)
-    anomaly_data[, anomaly := FALSE]
-    anomaly_data[outlier_idx, anomaly := TRUE]
-    p <- ggplot(data, aes_string(x = ts_var, y = variable)) +
+  outlier_dates <- list()
+  plots <- list()
+  data <- self$data
+
+  for (variable in varnames){
+    lower_bound <- quantile(data[[variable]], q, na.rm = TRUE)
+    upper_bound <- quantile(data[[variable]], 1-q, na.rm = TRUE)
+
+    outlier_idx <- which(data[[variable]] < lower_bound | data[[variable]] > upper_bound)
+
+    outlier_dates[[variable]] <- data[[outlier_idx, ..self$ts_var]]
+
+    p <- ggplot(data, aes_string(x = self$ts_var, y = variable)) +
       geom_line() +
-      geom_point(data = anomaly_data[anomaly == TRUE], aes_string(x = ts_var, y = variable, color = "anomaly")) +
-      labs(title = paste("Anomalies in", variable), x = "Time", y = variable) +
+      geom_point(data = anomaly_data[anomaly == TRUE], aes_string(x = self$ts_var, y = variable, color = "anomaly")) +
+      labs(title = paste("Outliers in", variable), x = "Time", y = variable) +
       scale_color_manual(values = c("black", "red")) +
       theme_minimal()
+
     plots[[variable]] <- p
-    outlier_dates[[variable]] <- data[outlier_idx, ..ts_var]
-    remove_row <- c(remove_row, outlier_idx)
+
+    if (winsorize) {
+      self$data[[variable]][self$data[[variable]] < lower_bound] <- lower_bound
+      self$data[[variable]][self$data[[variable]] > upper_bound] <- upper_bound
+    }
   }
-  remove_row <- unique(remove_row)
-  if (filter) {
-    data <- data[-remove_row, ]
-  }
-  return(list(data = data, outlier_dates = outlier_dates, remove_index = remove_row, plots = plots))
-  # Add to "ts" and "cs" method for panel data. Allow outlier detection by time-series and cross-sectional variables
-}
+
+  return(list(outlier_dates = outlier_dates, plots = plots))
+})
