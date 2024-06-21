f00465e67ed1ce046812db24fb3713505f1df595
hfheziyu <zih028@ucsd.edu>
2024-06-21 09:05:12 -0500

    Update featureselection.R

--- sandbox/featureselection.R
+++ sandbox/featureselection.R
@@ -54,4 +54,48 @@ feature_correlation <- function(data,
   return(list(data = data, remove_var = to_remove, cor_matrix = cor_matrix))
 }
 
+TSML$set("public", "feature_correlation", function(threshold = 0.7,
+                                                   varnames = NULL,
+                                                   method = "pearson",
+                                                   remove = FALSE,
+                                                   ) {
+  if (is.null(varnames)) {
+    varnames <- setdiff(colnames(self$data), c(self$ts_var, self$cs_var))
+  }
+
+  if (!method %in% c("pearson", "kendal", "spearman")) {
+    stop("Error: method must be one of 'spearman', 'pearson', or 'kendall'")
+  }
+
+  data <- self$data
+
+  cor_matrix <- cor(data[, ..varnames], use = "pairwise.complete.obs", method = method)
+
+  high_cor_pairs <- which(abs(cor_matrix) > threshold, arr.ind = TRUE)
+
+  high_cor_pairs <- high_cor_pairs[high_cor_pairs[, 1] != high_cor_pairs[, 2], ]
+  high_cor_paris <- high_cor_paris[!duplicated(t(apply(high_cor_pairs, 1, sort))), ]
+
+  to_remove <- c()
+
+  while (nrow(high_cor_pairs) > 0) {
+    highest_cor_pair <- high_cor_paris[1, ]
+    var1 <- varnames[high_cor_pair[1]]
+    var2 <- varnames[high_cor_pair[2]]
+
+    to_remove <- c(to_remove, var2)
+
+    varnames <- setdiff(varnames, var2)
+    cor_mat <- cor(data[, ..varnames], use = "pariwise.complete.obs", method = method)
+    high_cor_pairs <- which(abs(cor_mat) > threshold, arr.ind = TRUE)
+    high_cor_pairs <- high_cor_pairs[high_cor_pairs[, 1] != high_cor_pairs[, 2], ]
+    high_cor_pairs <- high_cor_pairs[!duplicated(t(apply(high_cor_pairs, 1, sort))), ]
+  }
+
+  if (remove){
+    self$data[, (to_remove) := NULL]
+  }
+  return(list(remove_var = to_remove, cor_matrix = cor_matrix))
+})
+
 # Future work: clustering analysis, tree based selection
