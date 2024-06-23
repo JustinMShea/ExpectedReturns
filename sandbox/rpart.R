1182de9446855d3494e71316b1621768e8f8de80
hfheziyu <zih028@ucsd.edu>
2024-06-23 17:23:57 -0400

    Update rpart.R

--- sandbox/rpart.R
+++ sandbox/rpart.R
@@ -12,19 +12,17 @@ test_rpart <- function(training_data,
                        testing_data,
                        y,
                        ...) {
-  mlr_rpart <- mlr_rpart(training_data, y, ...)
-  mlr_rpart_fitted <- mlr_rpart$model
-  mlr_rpart_predict <- mlr_rpart$predict_newdata(testing_data)[["response"]]
+  mlr_tree <- mlr_rpart(training_data, y, ...)
+  mlr_tree_fitted <- mlr_tree$model
+  mlr_tree_predict <- mlr_tree$predict_newdata(testing_data)[["response"]]
 
   formula <- paste(y, "~", paste(colnames(training_data[, !..y]), collapse = " + "))
-  rpart_rpart <- rpart::rpart(formula, training_data, ...)
-  rpart_predict <- unname(predict(rpart_rpart, testing_data))
+  rpart_tree <- rpart::rpart(formula, training_data, ...)
+  rpart_predict <- unname(predict(rpart_tree, testing_data))
 
-  if (identical(mlr_rpart_predict, rpart_predict)) {
+  if (identical(mlr_tree_predict, rpart_predict)) {
     print("rpart test passed.")
   } else {
     stop("Warning: rpart test failed.")
   }
 }
-
-
