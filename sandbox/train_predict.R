1952ce2e6e27b0632b02ac9242aa13b74328783d
hfheziyu <zih028@ucsd.edu>
2024-07-02 15:04:59 -0500

    Create train_predict.R

---
+++ sandbox/train_predict.R
@@ -0,0 +1,70 @@
+TSML$set("public", "train_predict", function(model,
+                                             method = c("default", "recursive"),
+                                             vars = NULL,
+                                             buffer = NULL,
+                                             ...) {
+  method <- method[1]
+  if (!method %in% c("default", "recursive")) {
+    stop("Error: method must be either 'default' or 'recursive'")
+  }
+  self$model <- model
+  self$learner <- lrn(model, ...)
+  self$prediction <- NULL
+  if (is.null(vars)) {
+    vars <- setdiff(colnames(self$data), c(self$ts_var, self$cs_var))
+  }
+  if (!self$y %in% vars) {
+    stop("Error: the target variable must be included in the variable list")
+  }
+  if (is.null(buffer)) {
+    current_train <- self$train_data[, ..vars]
+  } else {
+    if (!is.numeric(buffer)) {
+      stop("Error: buffer must be a number.")
+    }
+    if (buffer > nrow(self$train_data)) {
+      stop("Error: buffer must be smaller than the length of training data.")
+    }
+    buffer_idx <- -1:-(nrow(current_train) - buffer)
+    current_train <- self$train_data[buffer_idx, ..vars]
+  }
+
+  current_test <- self$test_data[, ..vars]
+  if (method == "default") {
+    if ("regr" %in% model) {
+      task <- as_task_regr(current_train, target = self$y)
+    } else if ("classif" %in% model) {
+      task <- as_task_classif(current_train, target = self$y)
+    }
+    self$learner$train(task)
+
+    self$prediction <- self$learner$predict_newdata(current_test)[["response"]]
+  }
+  if (method == "recursive") {
+    if ("regr" %in% model) {
+      task <- as_task_regr(current_train, target = self$y)
+    } else if ("classif" %in% model) {
+      task <- as_task_classif(current_train, target = self$y)
+    }
+
+    # This gets rid of the for loop, will have to test if it works
+    #recursive_predict <- function(index) {
+    #  new_test <- current_test[index, ]
+    #  self$learner$train(task)
+    #  prediction <- self$learner$predict_newdata(new_test)[["response"]]
+    #  current_train <<- rbind(current_train[-1, ], new_test)
+    #  task$backend <- current_train
+    #  return(prediction)
+    #}
+
+    self$prediction <- sapply(seq_len(nrow(current_test)), recursive_predict)
+
+    for (i in 1:nrow(current_test)) {
+      new_test <- current_test[i, ]
+      self$learner$train(task)
+      self$prediction[i] <- self$learner$predict_newdata(new_test)[["response"]]
+      current_train <- rbind(current_train[-1, ], new_test)
+      task$backend <- current_train
+    }
+  }
+})
