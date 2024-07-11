f68ab187288e797cd13d3f8e989829a46302f3a0
hfheziyu <zih028@ucsd.edu>
2024-07-11 09:36:49 +0800

    Update benchmark_strategy.R

--- sandbox/benchmark_strategy.R
+++ sandbox/benchmark_strategy.R
@@ -9,7 +9,14 @@ benchmark_strategy <- function(object,
   train_T <- length(unique(object$train_data[, ..ts_var]))
   test_T <- length(unique(object$test_data[, ..ts_var]))
 
-  train_end <- train_T - offset
+  train_end <- train_T
+  if (is.null(lookback)) {
+    train_start = 1
+  } else {
+    train_start = train_T - lookback + 1
+  }
+
+  current_train <- data[ts_var >= Time[train_start] | ts_var <= Time[train_end], ]
 
   N <- length(unique(object[, ..object$cs_var]))
 
@@ -19,6 +26,15 @@ benchmark_strategy <- function(object,
   w = w[1]
 
   if (w == "equal") {
-
+    portf_weights <- matrix(1/N, nrow = test_T, ncol = N)
+    for (t in 1:test_T) {
+      portf_returns[t, ] <- c(Time[train_T + t], sum(portf_weights[t, ] * current_test[, ..y]))
+    }
+  } else if (w == "scaled") {
+    for (t in 1:test_T) {
+      new_test <- current_test[ts_var == Time[train_T + t], ]
+      past_returns <- current_train[, lapply(y, DescTools::Gmean), by = object$cs_var]
+      portf_weights[t, ] <- past_returns / sum(predictions)
+    }
   }
 }
