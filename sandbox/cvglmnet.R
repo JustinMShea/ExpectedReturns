92e97180fd9ffadd5fe6c38a561544493ba057a9
hfheziyu <zih028@ucsd.edu>
2024-07-24 22:26:24 +0800

    Update cvglmnet.R

--- sandbox/cvglmnet.R
+++ sandbox/cvglmnet.R
@@ -2,7 +2,7 @@ mlr_cvglmnet <- function(training_data,
                      y,
                      alpha = 0.5) {
   enet <- TaskRegr$new(id = "enet", backend = training_data, target = y)
-  lrn_enet <- lrn("regr.cv_glmnet", alpha = alpha)
+  lrn_enet <- lrn("regr.cv_glmnet", alpha = alpha, s = "lambda.min")
   lrn_enet$train(enet)
   return(lrn_enet)
 }
@@ -17,12 +17,12 @@ test_cvglmnet <- function(training_data,
   mlr_enet_fitted <- mlr_enet$model
   mlr_enet_lambda_min <- mlr_enet_fitted$lambda.min
 
-  mlr_enet_predict <- mlr_enet$predict_newdata(testing_data, s = "lambda.min")
+  mlr_enet_predict <- mlr_enet$predict_newdata(testing_data)
 
   x_train <- as.matrix(training_data[, !..y])
   y_train <- training_data[[y]]
 
-  glm_enet <- cv.glmnet(x_train, y_train, alpha = alpha)
+  glm_enet <- cv.glmnet(x_train, y_train, alpha = alpha, family = "gaussian")
   glm_enet_lambda_min <- glm_enet$lambda.min
 
   glm_predict <- predict(glm_enet, newx = x_test, s = glm_enet_lambda_min)
