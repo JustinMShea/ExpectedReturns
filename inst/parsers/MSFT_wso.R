689ff823a36684ee0aee9e2614b6607cb671317e
apakr <megapuppypower@gmail.com>
2025-06-27 19:56:12 -0500

    made MSFT wso parser and saved MSFT_wo to data

---
+++ inst/parsers/MSFT_wso.R
@@ -0,0 +1,11 @@
+# Load qk library
+library(qkiosk)
+
+# May need to add qk API key into environment variables for data to be fetched. Free keys available online.
+
+# Get qk weighted shares outstanding and return file date and shares outstanding. Returns data frame.
+MSFT_wso <- as.data.frame(qk_fn(qk_ticker("MSFT"), "WSO")[])
+MSFT_wso <- na.omit(MSFT_wso[ MSFT_wso$fq > 0, c("fq","filed")])
+MSFT_wso$filed <- as.Date(as.character(MSFT_wso$filed), "%Y%m%d")
+
+save(MSFT_wso, file = "data/MSFT_wso.RData")
