d8689641874c958a1e184d5971247b680594ce59
apakr <megapuppypower@gmail.com>
2025-07-10 15:01:39 -0500

    MSFT_pePIT update

--- inst/parsers/MSFT_pePIT.R
+++ inst/parsers/MSFT_pePIT.R
@@ -4,11 +4,11 @@
 load("data/MSFT.RData")
 
 # attach eps data
-load("data/MSFT_eps.RData")
+load("data/MSFT_epsPIT.RData")
 
 # merged price and pe for date matching
-MSFT_pe_px <- merge(MSFT$MSFT.Adjusted, MSFT_eps)
-names(MSFT_pe_px) <- c("MSFT.Adjusted","EPS")
+MSFT_pe_px <- merge(MSFT$MSFT.Adjusted, MSFT_epsPIT)
+names(MSFT_pe_px) <- c("MSFT.Adjusted","epsPIT")
 MSFT_pe_px <- na.locf(MSFT_pe_px)
 MSFT_pe_px <- na.trim(MSFT_pe_px)
 
@@ -18,11 +18,11 @@ pe_ratio <- function(price, pe){
 }
 
 msft_pe <- pe_ratio(price = MSFT_pe_px$MSFT.Adjusted,
-                        pe = MSFT_pe_px$EPS)
+                        pe = MSFT_pe_px$epsPIT)
 
-colnames(msft_pe) <- "PEratio"
+colnames(msft_pe) <- "PEratioPIT"
 
-save(msft_pe, file = "data/MSFT_pe.RData")
+save(msft_pe, file = "data/MSFT_pePIT.RData")
 
 # KNOWN ISSUES - qk data has multiple fq measurements for certain singular dates, this causes the data to look
 # fine when plotted but in actuality theres some sort of issue.
