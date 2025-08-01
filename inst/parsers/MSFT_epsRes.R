7260acab29de1c7d753442d63e9aaf93e80f840e
apakr <megapuppypower@gmail.com>
2025-08-01 05:54:48 -0500

    epsRes data fix. now xts with numeric data

--- inst/parsers/MSFT_epsRes.R
+++ inst/parsers/MSFT_epsRes.R
@@ -1,5 +1,6 @@
-# Load qk library
+# Load libraries
 library(qkiosk)
+library(xts)
 
 # May need to add qk API key into environment variables for data to be fetched.
 # Free keys available online.
@@ -8,6 +9,9 @@ library(qkiosk)
 MSFT_epsRes <- as.data.frame(qk_fn(qk_ticker("MSFT"), "EPS")[]) # excludes as filed, includes Restatements
 MSFT_epsRes <- na.omit(MSFT_epsRes[, c("fq", "fpe")])
 MSFT_epsRes$fpe <- as.Date(as.character(MSFT_epsRes$fpe), "%Y%m%d")
+
+# convert to xts object
+MSFT_epsRes <- xts(as.numeric(MSFT_epsRes$fq), order.by = MSFT_epsRes$fpe)
 
 str(MSFT_epsRes)
 
