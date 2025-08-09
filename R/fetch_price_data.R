507d1ce9bca09142d2db49c1927d806b881d8411
apakr <megapuppypower@gmail.com>
2025-08-09 06:04:39 -0500

    namespace bulletproofing and deleted commented out code

--- R/fetch_price_data.R
+++ R/fetch_price_data.R
@@ -28,15 +28,13 @@
 #' @export
 
 library(quantmod)
+library(zoo)
+library(xts)
 
 fetch_price_data <- function(tickers, frequency = "daily") {
   if (!frequency %in% c("daily", "weekly", "monthly")) {
     stop("Frequency must be 'daily', 'weekly', or 'monthly'")
   }
-
-  # # makes char inputs into date inputs for quantmod data fetching
-  # if (!is.null(from)) from <- as.Date(from)
-  # if (!is.null(to))   to   <- as.Date(to)
 
   all_data <- list()
   failed_tickers <- c()
@@ -46,23 +44,23 @@ fetch_price_data <- function(tickers, frequency = "daily") {
     yahoo_ticker <- gsub("/", "-", ticker)  # for issues with symbol like BRK/B
 
     try_result <- tryCatch({ # use try catch so if one ticker doesn't work it doesn't mess up the whole loop, try_result will be TRUE when ticker data can be fetched, and FALSE when it cannot - which is put into failed tickers
-      data <- getSymbols(Symbols = yahoo_ticker,
+      data <- quantmod::getSymbols(Symbols = yahoo_ticker,
                          src = "yahoo",
                          auto.assign = FALSE)
-      adjusted <- Ad(data) # quantmod function to get adjusted close prices
+      adjusted <- quantmod::Ad(data) # quantmod function to get adjusted close prices
 
       # adjust frequency
       adjusted <- switch(frequency,
                          "daily" = adjusted,
-                         "weekly" = to.weekly(adjusted)[, 4],  # end period adjusted closes
-                         "monthly" = to.monthly(adjusted)[, 4])
+                         "weekly" = quantmod::to.weekly(adjusted)[, 4],  # end period adjusted closes
+                         "monthly" = quantmod::to.monthly(adjusted)[, 4])
       # so, interestingly, the to.weekly and to.monthly functions return OHLCs using only the data it's given, which is only the adj close
       # prices for the ticker, so the OHLC for the weekly/monthly may (will) miss the true opens, highs, and lows within the period,
       # but that isn't a problem for us because we only use the adjusted close price, which is the true one, so we don't care about the
       # OHL, only the C.
 
       # calc log returns
-      adj_vals <- coredata(adjusted) # coredata to extract numeric values from quantmod xts object
+      adj_vals <- zoo::coredata(adjusted) # coredata to extract numeric values from quantmod xts object
       log_ret <- c(NA, diff(log(adj_vals))) # pads the first value in the series as NA b/c you can't get a return for t = 0.
 
       df <- data.frame(
